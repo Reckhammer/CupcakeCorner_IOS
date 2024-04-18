36c73d588ee72d25a1c70530cd161d79bd16cfb5
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-04-18 18:09:38 -0500

    Step 3 creating a form with conditionally disabled features

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -11,8 +11,34 @@ struct ContentView: View
 {
     @State private var results = [Result]();
     
+    @State private var username = "";
+    @State private var email = "";
+    
+    var disableForm: Bool
+    {
+        username.count < 5 || email.count < 5;
+    }
+    
     var body: some View
     {
+        Form
+        {
+            Section
+            {
+                TextField("Username", text: $username);
+                TextField("Email", text: $email);
+            }
+            
+            Section
+            {
+                Button("Create Account")
+                {
+                    print("Creating account...");
+                }
+            }
+            .disabled(disableForm);
+        }
+        
         List(results, id: \.trackId)
         {   item in
             
