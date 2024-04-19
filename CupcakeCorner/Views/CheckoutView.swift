551a09b75ba30a8e5d82eaccef96d212a174f1bd
Reckhammer <joshua.rechkemmer48@gmail.com>
2024-04-18 19:07:18 -0500

    Step 7 Implemented entering an address

---
+++ CupcakeCorner/Views/CheckoutView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Joshua Rechkemmer on 4/18/24.
+//
+
+import SwiftUI
+
+struct CheckoutView: View 
+{
+    var order: Order;
+    
+    var body: some View
+    {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview 
+{
+    CheckoutView(order: Order());
+}
